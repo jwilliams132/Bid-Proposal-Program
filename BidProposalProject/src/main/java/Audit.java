93a07a3ecec4095f506e88a05e0f63b40daf1e30
jwilliams132 <jwilliams1399@gmail.com>
2023-02-11 14:39:32 -0600

    Update Audit.java

--- BidProposalProject/src/main/java/Audit.java
+++ BidProposalProject/src/main/java/Audit.java
@@ -4,53 +4,59 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 
 public class Audit {
+
+	private final boolean doAudit = false;
 	private final String AUDITFILENAME = String.format("BidProposalProject\\src\\main\\resources\\Audit.txt");
-//	private final String AUDITFILENAME = String.format("Audit %d-%d %d.%2d.%2d.txt",
-//			LocalDateTime.now().getMonthValue(), LocalDateTime.now().getDayOfMonth(), LocalDateTime.now().getHour(),
-//			LocalDateTime.now().getMinute(), LocalDateTime.now().getSecond());
+	// private final String AUDITFILENAME = String.format("Audit %d-%d
+	// %d.%2d.%2d.txt",
+	// LocalDateTime.now().getMonthValue(), LocalDateTime.now().getDayOfMonth(),
+	// LocalDateTime.now().getHour(),
+	// LocalDateTime.now().getMinute(), LocalDateTime.now().getSecond());
 
 	public Audit() {
-		
+
 	}
 
 	public void add(String addition) {
-	
-		if(addition == "Start")
-			addition = "\n##################################################\n"
-					+ "# .###############################################\n"
-					+ "#    %%%%%%%%%%%%%####%%%%%&%###%%%%%%%%%%%%%%%###\n"
-					+ "#  *##/           ###/       ###%              ###\n"
-					+ "#  *####(         ###        ###%              ###\n"
-					+ "#  *#######       ##%         ##%              ###\n"
-					+ "#   %#######%    ###.         %###            %###\n"
-					+ "#    %########% ,###          .###           %####\n"
-					+ "#    ,##########%%#/           ##%           #####\n"
-					+ "#     ###########              (#/          .#####\n"
-					+ "#     %##########  ##           #.          %#####\n"
-					+ "#     ,##########,%####         %           ######\n"
-					+ "#      ##########(#######                  .######\n"
-					+ "#      %###################                #######\n"
-					+ "#      .#####################.             #######\n"
-					+ "#       ################.,#####,          .#######\n"
-					+ "#       ################  #######*        %#######\n"
-					+ "#       ,##############*  #########(      ########\n"
-					+ "#        ##############    ###########   .########\n"
-					+ "#        %#############    ############% %########\n"
-					+ "#        .############     /############% ########\n"
-					+ "#         ###########%      ############(   %#####\n"
-					+ "#                                             %###\n"
-					+ "#                                               %#";
+		if (doAudit) {
+			if (addition == "Start")
+				addition = "\n##################################################\n"
+						+ "# .###############################################\n"
+						+ "#    %%%%%%%%%%%%%####%%%%%&%###%%%%%%%%%%%%%%%###\n"
+						+ "#  *##/           ###/       ###%              ###\n"
+						+ "#  *####(         ###        ###%              ###\n"
+						+ "#  *#######       ##%         ##%              ###\n"
+						+ "#   %#######%    ###.         %###            %###\n"
+						+ "#    %########% ,###          .###           %####\n"
+						+ "#    ,##########%%#/           ##%           #####\n"
+						+ "#     ###########              (#/          .#####\n"
+						+ "#     %##########  ##           #.          %#####\n"
+						+ "#     ,##########,%####         %           ######\n"
+						+ "#      ##########(#######                  .######\n"
+						+ "#      %###################                #######\n"
+						+ "#      .#####################.             #######\n"
+						+ "#       ################.,#####,          .#######\n"
+						+ "#       ################  #######*        %#######\n"
+						+ "#       ,##############*  #########(      ########\n"
+						+ "#        ##############    ###########   .########\n"
+						+ "#        %#############    ############% %########\n"
+						+ "#        .############     /############% ########\n"
+						+ "#         ###########%      ############(   %#####\n"
+						+ "#                                             %###\n"
+						+ "#                                               %#";
 
-		// Open the file for writing
-		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter(AUDITFILENAME, true));
-			// Write some text to the file
-			writer.write(String.format("%d//%d  %02d:%02d:%02d : %s", LocalDateTime.now().getMonthValue(), LocalDateTime.now().getDayOfMonth(),LocalDateTime.now().getHour(),
-					LocalDateTime.now().getMinute(), LocalDateTime.now().getSecond(), addition));
-			writer.newLine(); // Add a new line
-			writer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
+			// Open the file for writing
+			try {
+				BufferedWriter writer = new BufferedWriter(new FileWriter(AUDITFILENAME, true));
+				// Write some text to the file
+				writer.write(String.format("%d//%d  %02d:%02d:%02d : %s", LocalDateTime.now().getMonthValue(),
+						LocalDateTime.now().getDayOfMonth(), LocalDateTime.now().getHour(),
+						LocalDateTime.now().getMinute(), LocalDateTime.now().getSecond(), addition));
+				writer.newLine(); // Add a new line
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
