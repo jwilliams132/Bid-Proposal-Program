75720bc84945c4a0be542007bdc9dc5573d50b93
jwilliams132 <jwilliams1399@gmail.com>
2023-05-03 11:54:20 -0500

    meh

--- BidProposalProject/src/main/java/TestApp.java
+++ BidProposalProject/src/main/java/TestApp.java
@@ -22,9 +22,7 @@ import javax.swing.filechooser.FileFilter;
 
 import java.awt.FlowLayout;
 import java.awt.ComponentOrientation;
-import java.awt.Component;
 import java.awt.Point;
-import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
@@ -368,7 +366,7 @@ public class TestApp {
 						"BidProposalProject\\src\\main\\resources\\Test Template.xlsm",
 						null, FileManager.fileChooserOptions.OPEN, xslmFileFilter);
 
-				int startingEstimateNo = 1960;  // APR FINISHED WITH 2111
+				int startingEstimateNo = 2111;  // APR FINISHED WITH 2111
 				ExcelManager excelManager;
 
 				for (int jobIndex = 0; jobIndex < parseFullDoc.getJobList().size(); jobIndex++) {
