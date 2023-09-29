93f0f79cfe1e4224e82e6eef3286ca5b2c3a840d
jwilliams132 <jwilliams1399@gmail.com>
2023-09-29 14:29:06 -0500

    adding equals override

--- BidProposalProject/src/main/java/Contractor.java
+++ BidProposalProject/src/main/java/Contractor.java
@@ -1,140 +1,163 @@
 import java.util.regex.Pattern;
+import java.util.Objects;
 import java.util.regex.Matcher;
 
 public class Contractor implements Comparable<Contractor> {
 
-	private String contractorName;
-	private String contractorPhoneNumber;
-	private String contractorEmail;
+    private String contractorName;
+    private String contractorPhoneNumber;
+    private String contractorEmail;
+    private String contractorEstimateNumber;
 
-	public Contractor(String infoLine) {
+    public Contractor(String infoLine) {
 
-		setContractorName(findContractorName(infoLine)); // sets name from a trimmed substring of infoLine
-		setContractorPhoneNumber(findContractorPhoneNumber(infoLine)); // sets phone number from a regex of infoLine
-		setContractorEmail(findContractorEmail(infoLine)); // sets email from a regex search of infoLine
-	}
+        setContractorName(findContractorName(infoLine)); // sets name from a trimmed substring of infoLine
+        setContractorPhoneNumber(findContractorPhoneNumber(infoLine)); // sets phone number from a regex of infoLine
+        setContractorEmail(findContractorEmail(infoLine)); // sets email from a regex search of infoLine
+    }
 
-	public Contractor(String contractorName, String contractorPhoneNumber, String contractorEmail) {
+    public Contractor(String contractorName, String contractorPhoneNumber, String contractorEmail) {
 
-		this.contractorName = contractorName;
-		this.contractorPhoneNumber = contractorPhoneNumber;
-		this.contractorEmail = contractorEmail;
-	}
+        this.contractorName = contractorName;
+        this.contractorPhoneNumber = contractorPhoneNumber;
+        this.contractorEmail = contractorEmail;
+    }
 
-	public Contractor(String contractorName, String contractorPhoneNumber, String contractorEmail, String ContractorEstimateNo){
-		
-		// TODO
-	}
+    public Contractor(String contractorName, String contractorPhoneNumber, String contractorEmail,
+            String contractorEstimateNumber) {
 
-	// ====================================================================================================
-	// Parsing Data
-	// ====================================================================================================
+        // TODO
+    }
 
-	// finds the name of the contractor, by taking a substring and gets rid of the
-	// trailing '.'s
-	public String findContractorName(String infoLine) {
+    // ====================================================================================================
+    // Parsing Data
+    // ====================================================================================================
 
-		String nameLine = infoLine.substring(0, 35); // creates the substring from the first 34 characters
-		String name = nameLine.replaceAll("\\.{2,}", ""); // deletes the trailing '.'s
-		return name;
-	}
+    // finds the name of the contractor, by taking a substring and gets rid of the
+    // trailing '.'s
+    public String findContractorName(String infoLine) {
 
-	// uses regex to find email from infoLine
-	private String findContractorEmail(String infoLine) {
+        String nameLine = infoLine.substring(0, 35); // creates the substring from the first 34 characters
+        String name = nameLine.replaceAll("\\.{2,}", ""); // deletes the trailing '.'s
+        return name;
+    }
 
-		String email = ""; // buffer
-		Matcher emailMatcher = Pattern.compile("[A-Za-z0-9\\.]+@[A-Za-z-]+\\.[A-Za-z]+").matcher(infoLine); // uses
-																											// Pattern/Matcher
-																											// to search
-																											// through
-																											// the
-																											// string
-																											// using
-																											// regex.
+    // uses regex to find email from infoLine
+    private String findContractorEmail(String infoLine) {
 
-		// if regex finds the email, assign it to the buffer
-		if (emailMatcher.find()) {
-			email = emailMatcher.group(); // assign found email to the buffer
-			return email;
-		}
-		ContractorStorage storage = new ContractorStorage();
-		return storage.getEmail(contractorName); // returns a message if no email is found
-	}
+        String email = ""; // buffer
+        Matcher emailMatcher = Pattern.compile("[A-Za-z0-9\\.]+@[A-Za-z-]+\\.[A-Za-z]+").matcher(infoLine); // uses
+                                                                                                            // Pattern/Matcher
+                                                                                                            // to search
+                                                                                                            // through
+                                                                                                            // the
+                                                                                                            // string
+                                                                                                            // using
+                                                                                                            // regex.
 
-	// uses regex to find phone number from infoLine
-	private String findContractorPhoneNumber(String infoLine) {
+        // if regex finds the email, assign it to the buffer
+        if (emailMatcher.find()) {
+            email = emailMatcher.group(); // assign found email to the buffer
+            return email;
+        }
+        ContractorStorage storage = new ContractorStorage();
+        return storage.getEmail(contractorName); // returns a message if no email is found
+    }
 
-		String phone = ""; // buffer
-		Matcher phoneNumberMatcher = Pattern.compile("\\(*\\d{3}\\)*.{8}").matcher(infoLine); // uses Pattern/Matcher to
-																								// search through the
-																								// string using regex.
+    // uses regex to find phone number from infoLine
+    private String findContractorPhoneNumber(String infoLine) {
 
-		// if regex finds the phone number, assign it to the buffer
-		if (phoneNumberMatcher.find()) {
+        String phone = ""; // buffer
+        Matcher phoneNumberMatcher = Pattern.compile("\\(*\\d{3}\\)*.{8}").matcher(infoLine); // uses Pattern/Matcher to
+                                                                                              // search through the
+                                                                                              // string using regex.
 
-			phone = phoneNumberMatcher.group(); // assign found phone number to the buffer
-			return phone;
-		}
-		return "=============No Phone Found============="; // returns a message if no phone number is found
-	}
+        // if regex finds the phone number, assign it to the buffer
+        if (phoneNumberMatcher.find()) {
 
-	// ====================================================================================================
-	// Outputting Data
-	// ====================================================================================================
+            phone = phoneNumberMatcher.group(); // assign found phone number to the buffer
+            return phone;
+        }
+        return "=============No Phone Found============="; // returns a message if no phone number is found
+    }
 
-	public void printContractorInfo() {
+    // ====================================================================================================
+    // Outputting Data
+    // ====================================================================================================
 
-		System.out.printf("%-40s%-45s%40s%n", getContractorName(), getContractorPhoneNumber(), getContractorEmail());
-	}
+    public void printContractorInfo() {
 
-	public String formatContractorInfo() {
+        System.out.printf("%-40s%-45s%40s%n", getContractorName(), getContractorPhoneNumber(), getContractorEmail());
+    }
 
-		return String.format("|%s|%s|%s", getContractorName(), getContractorPhoneNumber(), getContractorEmail());
-	}
+    public String formatContractorInfo() {
 
-	// ====================================================================================================
-	// Getter Setters
-	// ====================================================================================================
+        return String.format("|%s|%s|%s", getContractorName(), getContractorPhoneNumber(), getContractorEmail());
+    }
 
-	public String getContractorName() {
+    // ====================================================================================================
+    // Getter Setters
+    // ====================================================================================================
 
-		return contractorName;
-	}
+    public String getContractorName() {
 
-	public void setContractorName(String contractorName) {
+        return contractorName;
+    }
 
-		this.contractorName = contractorName;
-	}
+    public void setContractorName(String contractorName) {
 
-	public String getContractorPhoneNumber() {
+        this.contractorName = contractorName;
+    }
 
-		return contractorPhoneNumber;
-	}
+    public String getContractorPhoneNumber() {
 
-	public void setContractorPhoneNumber(String contractorPhoneNumber) {
+        return contractorPhoneNumber;
+    }
 
-		this.contractorPhoneNumber = contractorPhoneNumber;
-	}
+    public void setContractorPhoneNumber(String contractorPhoneNumber) {
 
-	public String getContractorEmail() {
+        this.contractorPhoneNumber = contractorPhoneNumber;
+    }
 
-		return contractorEmail;
-	}
+    public String getContractorEmail() {
 
-	public void setContractorEmail(String contractorEmail) {
+        return contractorEmail;
+    }
 
-		this.contractorEmail = contractorEmail;
-	}
+    public void setContractorEmail(String contractorEmail) {
 
-	public String getContractorEstimateNo() {
+        this.contractorEmail = contractorEmail;
+    }
 
-		return null;
-	}
+    public String getContractorEstimateNo() {
 
-	@Override
-	public int compareTo(Contractor other) {
+        return null;
+    }
 
-		return this.contractorName.compareTo(other.contractorName);
-	}
+    @Override
+    public int compareTo(Contractor other) {
+
+        return this.contractorName.compareTo(other.contractorName);
+    }
+
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+
+            return false;
+        }
+
+        Contractor otherContractor = (Contractor) obj;
+
+        return Objects.equals(contractorName, otherContractor.contractorName) &&
+                Objects.equals(contractorPhoneNumber, otherContractor.contractorPhoneNumber) &&
+                Objects.equals(contractorEmail, otherContractor.contractorEmail) &&
+                Objects.equals(contractorEstimateNumber, otherContractor.contractorEstimateNumber);
+    }
 
 }
--- BidProposalProject/src/main/java/LineItem.java
+++ BidProposalProject/src/main/java/LineItem.java
@@ -1,56 +1,77 @@
+import java.util.Objects;
 
 public class LineItem {
-	
-	private String description;
-	private float quantity;
-	private float price = 0;
-	
-	public LineItem(String description, float quantity, float price) {
-		
-		setDescription(description);
-		setQuantity(quantity);
-		setPrice(price);
-	}
-	
-	// ====================================================================================================
-	// Outputting Data
-	// ====================================================================================================
-	
-	public String returnLineItems() {
-		
-		return String.format("%-40s%,11.0f    %1.2f%n", getDescription(), Float.valueOf(getQuantity()), getPrice());
-	}
-	
-	public String formatLineItems() {
-		
-		return String.format("|%s|%s|%1.2f", getDescription(), getQuantity(), getPrice());
-	}
-	
-	// ====================================================================================================
-	// Getter Setters
-	// ====================================================================================================
 
-	public String getDescription() {
-		return description;
-	}
+    private String description;
+    private float quantity;
+    private float price = 0;
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public LineItem(String description, float quantity, float price) {
 
-	public float getQuantity() {
-		return quantity;
-	}
+        setDescription(description);
+        setQuantity(quantity);
+        setPrice(price);
+    }
 
-	public void setQuantity(float quantity) {
-		this.quantity = quantity;
-	}
-	
-	public float getPrice() {
-		return price;
-	}
-	
-	public void setPrice(float price) {
-		this.price = price;
-	}
+    // ====================================================================================================
+    // Outputting Data
+    // ====================================================================================================
+
+    public String returnLineItems() {
+
+        return String.format("%-40s%,11.0f    %1.2f%n", getDescription(), Float.valueOf(getQuantity()), getPrice());
+    }
+
+    public String formatLineItems() {
+
+        return String.format("|%s|%s|%1.2f", getDescription(), getQuantity(), getPrice());
+    }
+
+    // ====================================================================================================
+    // Getter Setters
+    // ====================================================================================================
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(float quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            
+            return false;
+        }
+
+        LineItem otherLineItem = (LineItem) obj;
+
+        return Objects.equals(description, otherLineItem.description) &&
+                quantity == otherLineItem.quantity &&
+                Float.compare(otherLineItem.price, price) == 0;
+    }
 }
