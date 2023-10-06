a889a142b4a00dda544a52d3b75c7d2f6b593b5c
jwilliams132 <jwilliams1399@gmail.com>
2023-10-06 02:43:04 -0500

    finished?

--- BidProposalProject/src/main/java/V1ExcelFormat.java
+++ BidProposalProject/src/main/java/V1ExcelFormat.java
@@ -4,64 +4,46 @@ public class V1ExcelFormat extends ExcelFormat {
 
     private final String templateFileName = "BidProposalProject\\src\\main\\resources\\Test Template.xlsm";
     private int startingEstimateNo = 1234;
+
     private String estimateNumber = "";
 
-    private final int CSJCOLUMN = 6;
-    private final int CSJROW = 3;
-
-    private final int HIGHWAYCOLUMN = 0;
-    private final int HIGHWAYROW = 30;
-
-    private final int COUNTYCOLUMN = 0;
-    private final int COUNTYROW = 33;
-
-    private final int ADDMOBSCOLUMN = 3;
-    private final int ADDMOBSROW = 23;
-
-    private final int TOTALMOBSCOLUMN = 6;
-    private final int TOTALMOBSROW = 20;
-
-    private final int UPTOMOBSCOLUMN = 4;
-    private final int UPTOMOBSROW = 20;
-
-    private final int ESTIMATENOCOLUMN = 27;
-    private final int ESTIMATENOROW = 0;
-
-    private final int CONTNAMECOLUMN = 0;
-    private final int CONTNAMEROW = 18;
-
-    private final int CONTPHONECOLUMN = 0;
-    private final int CONTPHONEROW = 22;
-
-    private final int CONTEMAILCOLUMN = 0;
-    private final int CONTEMAILROW = 24;
-
-    private final int SENTTOCOLUMN = 4;
-    private final int SENTTOROW = 3;
-
-    private final int LIQUANTITYCOLUMN = 3;
-    private final int LIQUANTITYROW = 7;
-
-    private final int LIDESCRIPTIONCOLUMN = 4;
-    private final int LIDISCRIPTIONROW = 7;
-
-    private final int LIPRICECOLUMN = 5;
-    private final int LIPRICEROW = 7;
-
-    private final int LITOTALCOLUMN = 6;
-    private final int LITOTALROW = 7;
-
-    private final int TOTALAMOUNTCOLUMN = 6;
-    private final int TOTALAMOUNTROW = 36;
+    private final CellCoordinates CSJ = new CellCoordinates(6, 3);
+    private final CellCoordinates HIGHWAY = new CellCoordinates(0, 30);
+    private final CellCoordinates COUNTY = new CellCoordinates(0, 33);
+    private final CellCoordinates ADDMOBS = new CellCoordinates(3, 23);
+    private final CellCoordinates TOTALMOBS = new CellCoordinates(6, 20);
+    private final CellCoordinates UPTOMOBS = new CellCoordinates(4, 20);
+    private final CellCoordinates ESTIMATENO = new CellCoordinates(27, 0);
+    private final CellCoordinates CONTNAME = new CellCoordinates(0, 18);
+    private final CellCoordinates CONTPHONE = new CellCoordinates(0, 22);
+    private final CellCoordinates CONTEMAIL = new CellCoordinates(0, 24);
+    private final CellCoordinates SENTTO = new CellCoordinates(4, 3);
+    private final CellCoordinates LIQUANTITY = new CellCoordinates(3, 7);
+    private final CellCoordinates LIDESCRIPTION = new CellCoordinates(4, 7);
+    private final CellCoordinates LIPRICE = new CellCoordinates(5, 7);
+    private final CellCoordinates LITOTAL = new CellCoordinates(6, 7);
+    private final CellCoordinates TOTALAMOUNT = new CellCoordinates(6, 36);
 
     @Override
-    public void createExcelFile() {
+    public void createExcelFile(List<Job> jobs, String lettingMonthDirectory) {
 
-        createWorkBook(templateFileName);
-        populateExcel(null);
+        int contractorNumber = 0;
+        // do something for estimate #
+        for (Job job : jobs) {
+
+            contractorNumber++;
+            createWorkBook(templateFileName);
+            populateExcel(job, getEstimateNo(lettingMonthDirectory), contractorNumber);
+            saveWorkbook(String.format("%s\\%S %s%s", lettingMonthDirectory,
+            job.getCounty(),
+            job.getCsj(), ".xlsm"));
+        }
 
     }
 
+    // excelManager.saveWorkbook(String.format("%s\\%S %s%s", lettingMonthDirectory,
+    //                 parseFullDoc.getJobList().get(jobIndex).getCounty(),
+    //                 parseFullDoc.getJobList().get(jobIndex).getCsj(), ".xlsm"));
     @Override
     public List<Job> getInfoFromExcelFile() {
 
@@ -69,7 +51,7 @@ public class V1ExcelFormat extends ExcelFormat {
     }
 
     @Override
-    public void populateExcel(Job job) {
+    public void populateExcel(Job job, String estimateString, int contractorNumber) {
 
         String sheetName;
         Contractor contractor;
@@ -84,20 +66,20 @@ public class V1ExcelFormat extends ExcelFormat {
             sheetName = String.valueOf(contractorIndex + 1);
             contractor = job.getContractorList().get(contractorIndex);
 
-            setCellValue(sheetName, CSJCOLUMN, CSJROW, job.getCsj());
-            setCellValue(sheetName, HIGHWAYCOLUMN, HIGHWAYROW, job.getHighway());
-            setCellValue(sheetName, COUNTYCOLUMN, COUNTYROW, job.getCounty());
-            setCellValue(sheetName, ADDMOBSCOLUMN, ADDMOBSROW, job.getAdditionalMobs());
-            setCellValue(sheetName, TOTALMOBSCOLUMN, TOTALMOBSROW, job.getTotalMobs());
-            setCellValue(sheetName, UPTOMOBSCOLUMN, UPTOMOBSROW, job.getUpTo_Mobs());
-            setCellValue(sheetName, ESTIMATENOCOLUMN, ESTIMATENOROW,
-                    String.format("%s%d", "WR-2023-", estimateNumber + contractorIndex));
+            setCellValue(sheetName, CSJ.column, CSJ.row, job.getCsj());
+            setCellValue(sheetName, HIGHWAY.column, HIGHWAY.row, job.getHighway());
+            setCellValue(sheetName, COUNTY.column, COUNTY.row, job.getCounty());
+            setCellValue(sheetName, ADDMOBS.column, ADDMOBS.row, job.getAdditionalMobs());
+            setCellValue(sheetName, TOTALMOBS.column, TOTALMOBS.row, job.getTotalMobs());
+            setCellValue(sheetName, UPTOMOBS.column, UPTOMOBS.row, job.getUpTo_Mobs());
+            setCellValue(sheetName, ESTIMATENO.column, ESTIMATENO.row,
+                    String.format("%s%d", estimateString, contractorNumber));
 
             totalAmount += job.getTotalMobs();
-            setCellValue(sheetName, CONTNAMECOLUMN, CONTNAMEROW, contractor.getContractorName());
-            setCellValue(sheetName, CONTEMAILCOLUMN, CONTEMAILROW, contractor.getContractorEmail());
-            setCellValue(sheetName, SENTTOCOLUMN, SENTTOROW, contractor.getContractorEmail());
-            setCellValue(sheetName, CONTPHONECOLUMN, CONTPHONEROW, contractor.getContractorPhoneNumber());
+            setCellValue(sheetName, CONTNAME.column, CONTNAME.row, contractor.getContractorName());
+            setCellValue(sheetName, CONTEMAIL.column, CONTEMAIL.row, contractor.getContractorEmail());
+            setCellValue(sheetName, SENTTO.column, SENTTO.row, contractor.getContractorEmail());
+            setCellValue(sheetName, CONTPHONE.column, CONTPHONE.row, contractor.getContractorPhoneNumber());
 
             for (int lineItemIndex = 0; lineItemIndex < job.getLineItems().size(); lineItemIndex++) {
 
@@ -105,15 +87,14 @@ public class V1ExcelFormat extends ExcelFormat {
                 lineItemAmount = lineItem.getQuantity() * lineItem.getPrice();
                 totalAmount += lineItemAmount;
 
-                setCellValue(sheetName, LIQUANTITYCOLUMN, LIQUANTITYROW + lineItemIndex, lineItem.getQuantity());
-                setCellValue(sheetName, LIDESCRIPTIONCOLUMN, LIDISCRIPTIONROW + lineItemIndex,
+                setCellValue(sheetName, LIQUANTITY.column, LIQUANTITY.row + lineItemIndex, lineItem.getQuantity());
+                setCellValue(sheetName, LIDESCRIPTION.column, LIDESCRIPTION.row + lineItemIndex,
                         lineItem.getDescription());
-                setCellValue(sheetName, LIPRICECOLUMN, LIPRICEROW + lineItemIndex, lineItem.getPrice());
-                setCellValue(sheetName, LITOTALCOLUMN, LITOTALROW + lineItemIndex,
+                setCellValue(sheetName, LIPRICE.column, LIPRICE.row + lineItemIndex, lineItem.getPrice());
+                setCellValue(sheetName, LITOTAL.column, LITOTAL.row + lineItemIndex,
                         String.format("$%,1.2f", lineItemAmount));
             }
-            setCellValue(sheetName, TOTALAMOUNTCOLUMN, TOTALAMOUNTROW, String.format("$%,1.2f", totalAmount));
+            setCellValue(sheetName, TOTALAMOUNT.column, TOTALAMOUNT.row, String.format("$%,1.2f", totalAmount));
         }
     }
-
 }
